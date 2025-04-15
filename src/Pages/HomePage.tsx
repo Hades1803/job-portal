14e9f4b85562358022941949ea7fdf47a0a8b483
Quốc Nguyễn <anhquoc3241@gmail.com>
2025-04-15 15:41:29 +0700

    feat: complete HomePage UI and add Header component

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,11 +1,23 @@
+import { Footer } from '../Footer/Footer'
 import Header from '../Header/Header'
+import Companies from '../LandingPage/Companies'
 import DreamJob from '../LandingPage/DreamJob'
+import JobCategory from '../LandingPage/JobCategory'
+import Subcribe from '../LandingPage/Subcribe'
+import Testimonials from '../LandingPage/Testimonials'
+import Working from '../LandingPage/Working'
 
 const HomePage = () => {
   return (
     <div className='min-h-[100vh] bg-mine-shaft-950 font-["poppins"]'>
       <Header />
       <DreamJob/>
+      <Companies/>
+      <JobCategory/>
+      <Working/>
+      <Testimonials/>
+      <Subcribe/>
+      <Footer/>
     </div>
 
   )
